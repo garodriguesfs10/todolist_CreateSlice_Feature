d52051e7d36ab0b8627255c97817d0b4cfdfd39a
Gabriel Rodrigues Ramos de Oliveira <gabriel.oliveira@unimedlins.int.br>
2020-07-10 16:02:01 -0300

    get Mode

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { ADD_TODO, SAVE_EDIT_TODO, CLEAR_EDIT } from '../reducers/todoreducer'
+import { current } from '@reduxjs/toolkit'
 
 
 export const Add = () => {
@@ -19,6 +20,8 @@ export const Add = () => {
         }
     }, [currentTodo])
     // const teste = useSelector(state => state.teste)
+
+
 
     const HandleChange = (e) => {
         setForm({ ...form, task: e.target.value })
@@ -41,11 +44,22 @@ export const Add = () => {
         }
     }
 
+    function getMode() {
+        if (currentTodo.id !== undefined) {
+            return "Editando..."
+        } else {
+            return "Adicionando..."
+        }
+
+    }
+
     return (
         <>
+
             <hr />
+            <h4>Modo: {getMode()}</h4>
             <input type='text' placeholder="Adicionar tarefa" name='task' className="form-control" id='task' onChange={HandleChange} value={form.task || ''} required />
-            <br/>
+            <br />
             <button type='button' onClick={HandleSubmit} className='btn btn-success'>Adicionar</button>
 
         </>
